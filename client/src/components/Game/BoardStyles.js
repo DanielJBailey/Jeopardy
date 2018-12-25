4cc77884c3907cf9986ac4329916357d768584cb
Daniel Bailey <daniel.justin.bailey@gmail.com>
2018-12-24 23:43:22 -0700

    refactor board styles to another file

---
+++ client/src/components/Game/BoardStyles.js
@@ -0,0 +1,143 @@
+import styled from 'styled-components';
+
+export const MoneyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: linear-gradient(to top, #4a69bd, #1e3799, #1e3799, #4a69bd);
+  align-items: center;
+  width: 50%;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 900;
+  font-size: 45px;
+  color: white;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+`
+
+export const ScoreBoard = styled.div`
+  height: 100px;
+  width: 600px;
+  background-color: #3b3131;
+  box-shadow: inset 4px 4px 10px rgba(0, 0, 0, 0.3),
+    3px 5px 20px rgba(0, 0, 0, 0.3);
+  position: absolute;
+  padding: 5px;
+  top: 0;
+  right: 0;
+  display: flex;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+`
+
+export const NameContainer = styled.div`
+  width: 50%;
+  height: 100%;
+  margin-right: 10px;
+  background-color: #1e3799;
+  background: linear-gradient(to top, #4a69bd, #1e3799, #1e3799, #4a69bd);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+  align-items: center;
+  font-family: 'Shadows Into Light', cursive;
+  color: white;
+  font-size: 45px;
+`
+
+export const ChoicesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: auto;
+  width: 100%;
+
+  ul {
+    height: 100%;
+    flex: 1;
+    list-style: none;
+    margin: 0 5px;
+    &:first-child {
+      margin: 0 5px 0 0;
+    }
+    &:last-child {
+      margin: 0 0 0 5px;
+    }
+    li {
+      display: block;
+      background-color: #0c2461;
+      font-family: 'Roboto', sans-serif;
+      font-size: 50px;
+      color: yellow;
+      font-weight: bold;
+      height: 100px;
+      box-shadow: inset -5px -5px 10px rgba(0, 0, 0, 0.7),
+        inset 5px 5px 5px #1e3799;
+      display: flex;
+      flex-direction: column;
+      cursor: pointer;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 10px;
+      &:hover {
+        background-color: #1e3799;
+      }
+      &:last-child {
+        margin-bottom: 0px;
+      }
+    }
+  }
+`
+
+export const GameContainer = styled.div`
+  height: 100%;
+  width: 100vw;
+  min-height: 100vh;
+  overflow: hidden;
+  background: linear-gradient(to bottom left, #4776e6, #8e54e9);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .logo {
+    max-width: 300px;
+    margin-bottom: 20px;
+  }
+`
+
+export const GameBoard = styled.div`
+  width: 75vw;
+  height: auto;
+  background-color: black;
+  box-shadow: inset 5px 5px 5px rgba(255, 255, 255, 0.2),
+    5px 5px 20px rgba(0, 0, 0, 0.4);
+  border: 10px solid black;
+  border-radius: 5px;
+`
+
+export const CategoriesContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-gap: 10px;
+  border-bottom: 15px solid black;
+  font-family: 'Roboto', sans-serif;
+`
+
+export const Category = styled.div`
+  background-color: #0c2461;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-weight: bold;
+  font-size: 22px;
+  color: white;
+  padding: 10px;
+  box-shadow: inset -5px -5px 10px rgba(0, 0, 0, 0.7), inset 5px 5px 5px #1e3799;
+`
--- client/src/components/Game/Jeopardy.js
+++ client/src/components/Game/Jeopardy.js
@@ -1,97 +1,76 @@
 import React from 'react'
-import styled from 'styled-components'
 import logo from '../../assets/logo.png'
-import {getCategories} from '../../reducers/categories';
-// import {getCards} from '../../reducers/cards';
-import {connect} from 'react-redux';
-import axios from 'axios';
+import { getCategories } from '../../reducers/categories'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import {
+  Category,
+  CategoriesContainer,
+  GameBoard,
+  GameContainer,
+  ChoicesContainer,
+  NameContainer,
+  ScoreBoard,
+  MoneyContainer
+} from './BoardStyles'
 
 class Jeopardy extends React.Component {
   state = {
     money: 0,
     cards: [],
-    playerName: "Daniel"
+    playerName: 'Daniel',
+    renderCards: false
   }
 
-  componentDidMount() {
-      let {dispatch} = this.props;
-      dispatch(getCategories(this.getCards))
+  componentDidMount () {
+    const { dispatch } = this.props
+    dispatch(getCategories(this.getCards))
   }
 
   getCards = () => {
-    console.log('made it');
-    let {categories} = this.props;
-    let {cards} = this.state;
-    let allCards = [];
-    for(var i in categories) {
-        axios.get(`/api/categories/${categories[i].id}/cards`)
-        .then(res => {
-            for(var i = 0; i < res.data.length; i++) {
-                allCards.push(res.data[i]);
-            }
-        })
+    const { categories } = this.props
+    const { cards } = this.state
+    let allCards = []
+    for (var i in categories) {
+      axios.get(`/api/categories/${categories[i].id}/cards`).then(res => {
+        for (var i = 0; i < res.data.length; i++) {
+          allCards.push(res.data[i])
+        }
+      })
     }
     this.setState({
-        cards: allCards
+      cards: allCards,
+      renderCards: true
+    })
+  }
+
+  populateCards = index => {
+    let { cards } = this.state
+    console.log(cards.slice(index))
+    cards.slice(index).map(card => {
+      return <li key={card.id}>{card.dollar_value}</li>
     })
   }
 
   render () {
-      let {money } = this.state;
-      let {categories} = this.props;
+    let { money } = this.state
+    let { categories } = this.props
+    let { renderCards } = this.state
+
     return (
       <GameContainer>
         <img src={logo} alt='game-logo' className='logo' />
         <GameBoard>
           <CategoriesContainer>
-            {categories.map(cat => 
-                <Category key={cat.id}>{cat.name.toUpperCase()}</Category>
-            )}
+            {categories.map(cat => (
+              <Category key={cat.id}>{cat.name.toUpperCase()}</Category>
+            ))}
           </CategoriesContainer>
-          <ChoicesContainer>
-            <ul>
-              <li>$200</li>
-              <li>$400</li>
-              <li>$600</li>
-              <li>$800</li>
-              <li>$1000</li>
-            </ul>
-            <ul>
-              <li>$200</li>
-              <li>$400</li>
-              <li>$600</li>
-              <li>$800</li>
-              <li>$1000</li>
-            </ul>
-            <ul>
-              <li>$200</li>
-              <li>$400</li>
-              <li>$600</li>
-              <li>$800</li>
-              <li>$1000</li>
-            </ul>
-            <ul>
-              <li>$200</li>
-              <li>$400</li>
-              <li>$600</li>
-              <li>$800</li>
-              <li>$1000</li>
-            </ul>
-            <ul>
-              <li>$200</li>
-              <li>$400</li>
-              <li>$600</li>
-              <li>$800</li>
-              <li>$1000</li>
-            </ul>
-            <ul>
-              <li>$200</li>
-              <li>$400</li>
-              <li>$600</li>
-              <li>$800</li>
-              <li>$1000</li>
-            </ul>
-          </ChoicesContainer>
+          {renderCards ? (
+            <ChoicesContainer>
+              <ul>{this.populateCards(0, 4)}</ul>
+            </ChoicesContainer>
+          ) : null}
         </GameBoard>
         <ScoreBoard>
           <NameContainer>{this.state.playerName}</NameContainer>
@@ -102,150 +81,8 @@ class Jeopardy extends React.Component {
   }
 }
 
-const MoneyContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background: linear-gradient(to top, #4a69bd, #1e3799, #1e3799, #4a69bd);
-  align-items: center;
-  width: 50%;
-  font-family: 'Roboto', sans-serif;
-  font-weight: 900;
-  font-size: 45px;
-  color: white;
-  border-top-right-radius: 10px;
-  border-bottom-right-radius: 10px;
-`
-
-const ScoreBoard = styled.div`
-  height: 100px;
-  width: 600px;
-  background-color: #3b3131;
-  box-shadow: inset 4px 4px 10px rgba(0, 0, 0, 0.3),
-    3px 5px 20px rgba(0, 0, 0, 0.3);
-  position: absolute;
-  padding: 5px;
-  top: 0;
-  right: 0;
-  display: flex;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-`
-
-const NameContainer = styled.div`
-  width: 50%;
-  height: 100%;
-  margin-right: 10px;
-  background-color: #1e3799;
-  background: linear-gradient(to top, #4a69bd, #1e3799, #1e3799, #4a69bd);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  border-top-left-radius: 10px;
-  border-bottom-left-radius: 10px;
-  align-items: center;
-  font-family: 'Shadows Into Light', cursive;
-  color: white;
-  font-size: 45px;
-`
-
-const ChoicesContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  height: auto;
-  width: 100%;
-
-  ul {
-    height: 100%;
-    flex: 1;
-    list-style: none;
-    margin: 0 5px;
-    &:first-child {
-      margin: 0 5px 0 0;
-    }
-    &:last-child {
-      margin: 0 0 0 5px;
-    }
-    li {
-      display: block;
-      background-color: #0c2461;
-      font-family: 'Roboto', sans-serif;
-      font-size: 50px;
-      color: yellow;
-      font-weight: bold;
-      height: 100px;
-      box-shadow: inset -5px -5px 10px rgba(0, 0, 0, 0.7),
-        inset 5px 5px 5px #1e3799;
-      display: flex;
-      flex-direction: column;
-      cursor: pointer;
-      justify-content: center;
-      align-items: center;
-      margin-bottom: 10px;
-      &:hover {
-        background-color: #1e3799;
-      }
-      &:last-child {
-        margin-bottom: 0px;
-      }
-    }
-  }
-`
-
-const GameContainer = styled.div`
-  height: 100%;
-  width: 100vw;
-  min-height: 100vh;
-  overflow: hidden;
-  background: linear-gradient(to bottom left, #4776e6, #8e54e9);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  .logo {
-    max-width: 300px;
-    margin-bottom: 20px;
-  }
-`
-
-const GameBoard = styled.div`
-  width: 75vw;
-  height: auto;
-  background-color: black;
-  box-shadow: inset 5px 5px 5px rgba(255, 255, 255, 0.2),
-    5px 5px 20px rgba(0, 0, 0, 0.4);
-  border: 10px solid black;
-  border-radius: 5px;
-`
-
-const CategoriesContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-  grid-gap: 10px;
-  border-bottom: 15px solid black;
-  font-family: 'Roboto', sans-serif;
-`
-
-const Category = styled.div`
-  background-color: #0c2461;
-  height: 100px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  font-weight: bold;
-  font-size: 22px;
-  color: white;
-  padding: 10px;
-  box-shadow: inset -5px -5px 10px rgba(0, 0, 0, 0.7), inset 5px 5px 5px #1e3799;
-`
-
 const mapStateToProps = state => {
-    return { categories: state.categories, cards: state.cards }
+  return { categories: state.categories, cards: state.cards }
 }
 
 export default connect(mapStateToProps)(Jeopardy)
